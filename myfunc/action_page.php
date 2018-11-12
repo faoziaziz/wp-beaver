8a87b8d03a7f846e77de3b540168371c59fd7292
Aziz Amerul Faozi <admin@labseni.com>
2018-11-12 18:57:29 +0700

    Numble

---
+++ myfunc/action_page.php
@@ -0,0 +1,6 @@
+<?php
+    function test()
+    {
+        echo "halo semua";
+    }
+?> 
--- myfunc/asek.php
+++ myfunc/asek.php
@@ -3,4 +3,21 @@
   {
     echo "<h2>Hello Gue</h2>";
   }
+  function formulir()
+  {
+    ?>
+    <form action="action_page.php" method="post">
+        <fieldset>
+            <legend>Informasi Personal</legend>
+            Nama Depan : <br>
+            <input type="text" name="namadepan">
+            Nama Belakang: <br>
+            <input type="text" name="namabelakang">
+            <input type="submit" value="Submit">
+        </fieldset>
+    </form>
+    
+    <?php
+    
+  }
 ?>
